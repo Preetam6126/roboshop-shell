697591a95d5aed9b10d576ec04e9c648e101160c
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-22 10:18:58 +0000

    Added config to frontend,mongodb,catalogue

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+cp config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.com
 systemctl enable nginx 
 systemctl start nginx 
-systemctl status nginx
