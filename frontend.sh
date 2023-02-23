fd925d4fb7110527644d92f172ecd92fc27178a0
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-23 10:16:53 +0000

    frontend copy issue and redish file

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,11 @@
+code_dir=$(pwd)
 echo -e "\e[36mInstalling Nginx\e[0m"
 yum install nginx -y 
 
 echo -e "\e[36mRemoving files old contents\e[0m"
 rm -rf /usr/share/nginx/html/* 
 
-echo-e "\e[36mDownloading Frontend Content\e[0m"
+echo -e "\e[36mDownloading Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 
 echo -e "\e[36mExtracting Downloaded Frontend\e[0m"
@@ -12,7 +13,9 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 echo -e "\e[36mCopying Nginx Config for Roboshop\e[0m"
-cp config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.com
+
+cp ${code_dir}/config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.com
+
 
 echo -e "\e[36mEnabling and Starting Nginx\e[0m"
 systemctl enable nginx 
