5964f9d46dcadc46c8a50d3926d961517209512d
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-23 12:57:26 +0000

    sending errors and logs to the log_file

--- frontend.sh
+++ frontend.sh
@@ -1,23 +1,26 @@
 code_dir=$(pwd)
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
+
 echo -e "\e[36mInstalling Nginx\e[0m"
-yum install nginx -y 
+yum install nginx -y &>>${log_file}
 
 echo -e "\e[36mRemoving files old contents\e[0m"
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>${log_file}
 
 echo -e "\e[36mDownloading Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file} 
 
 echo -e "\e[36mExtracting Downloaded Frontend\e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log_file}
 
 echo -e "\e[36mCopying Nginx Config for Roboshop\e[0m"
 
-cp ${code_dir}/config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.com
+cp ${code_dir}/config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.com &>>${log_file}
 
 
 echo -e "\e[36mEnabling and Starting Nginx\e[0m"
-systemctl enable nginx 
-systemctl start nginx 
+systemctl enable nginx &>>${log_file}
+systemctl start nginx  &>>${log_file}
 
