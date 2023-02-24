e707a66337a85bfc2dca5107ca31ff532c718d6c
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-24 12:26:36 +0000

    Applied functions,sed command to mogodb

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,49 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app 
-rm -rf /app/*
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
-cd /app 
-unzip /tmp/catalogue.zip
-npm install
-cp config/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue 
-systemctl start catalogue
-cp config/mongodb.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb.devops36.shop </app/schema/catalogue.js
+source=common.sh
+
+print_head "Downloading Repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+print_head "Installing Nodejs"
+yum install nodejs -y &>>${log_file}
+
+print_head "Adding User Roboshop"
+useradd roboshop &>>${log_file}
+
+print_head "creating Apllication Directory"
+mkdir /app &>>${log_file}
+
+print_head "cleaning old files"
+rm -rf /app/* &>>${log_file}
+
+print_head "Downloading App Content zip file"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+
+print_head "changing Directory"
+cd /app &>>${log_file}
+
+print_head "Extracting App content"
+unzip /tmp/catalogue.zip &>>${log_file}
+
+print_head " installing nodejs support files"
+npm install &>>${log_file}
+
+print_head "copying service file to systemd "
+cp ${code_dir}/config/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+
+print_head "systemctl reload"
+systemctl daemon-reload &>>${log_file}
+
+print_head "Enable catalogue service"
+systemctl enable catalogue &>>${log_file}
+
+print_head "Start catalogue service"
+systemctl start catalogue &>>${log_file}
+
+print_head "copy mongodb repo files"
+cp ${cod_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+print_head "install mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+
+print_head "load schema to mongodb"
+mongo --host mongodb.devops36.shop </app/schema/catalogue.js &>>${log_file}
--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,18 @@
-echo -e "\e[35mcopying files to repository\e[0m"
-cp config/mongodb.repo /etc/yum.repos.d/mongo.repo
+source common.sh
 
-echo -e "\e[35mInstalling mongodb\e[0m"
-yum install mongodb-org -y 
+print_head "Setup MongoDB repository"
+cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo
 
-echo -e "\e[35m enabling and starting mongodb\e[0m"
-systemctl enable mongod 
-systemctl start mongod 
+print_head "Installing mongodb"
+yum install mongodb-org -y &>>${log_file}
+
+print_head "Update MongoDB Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongodb.conf
+
+print_head "Enabling mongodb service"
+systemctl enable mongod &>>${log_file}
+
+print_head "start mongodb service"
+systemctl restart mongod &>>${log_file}
 
 #update /etc/mongod.conf file from 127.0.0.1 to 0.0.0.0
