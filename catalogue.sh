4606f6790220aa52d923787664b2d688b1d41872
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-25 15:51:22 +0000

    added status check

--- catalogue.sh
+++ catalogue.sh
@@ -2,48 +2,64 @@ source common.sh
 
 print_head "Downloading Repository"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+echo $?
 
 print_head "Installing Nodejs"
 yum install nodejs -y &>>${log_file} 
+echo $?
 
 print_head "Adding User Roboshop"
 useradd roboshop &>>${log_file}
+echo $?
 
 print_head "creating Apllication Directory"
 mkdir /app &>>${log_file}
+echo $?
 
 print_head "cleaning old files"
 rm -rf /app/* &>>${log_file}
+echo $?
 
 print_head "Downloading App Content zip file"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
+echo $?
 
 print_head "changing Directory"
 cd /app &>>${log_file}
+echo $?
 
 print_head "Extracting App content"
 unzip /tmp/catalogue.zip &>>${log_file}
+echo $?
 
 print_head " installing nodejs support files"
 npm install &>>${log_file}
+echo $?
 
 print_head "copying service file to systemd "
 cp ${code_dir}/config/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
+echo $?
 
 print_head "systemctl reload"
 systemctl daemon-reload &>>${log_file}
+echo $?
 
 print_head "Enable catalogue service"
 systemctl enable catalogue &>>${log_file}
+echo $?
 
 print_head "Start catalogue service"
 systemctl start catalogue &>>${log_file}
+echo $?
 
 print_head "copy mongodb repo files"
 cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+echo $?
 
 print_head "install mongodb"
 yum install mongodb-org-shell -y &>>${log_file}
+echo $?
 
 print_head "load schema to mongodb"
 mongo --host mongodb.devops36.shop </app/schema/catalogue.js &>>${log_file}
+echo $?
--- frontend.sh
+++ frontend.sh
@@ -2,23 +2,26 @@ source common.sh
 
 print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
+status_check $?
 
 print_head "Removing files old contents"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
+status_check $?
 
 print_head "Downloading Frontend Content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file} 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "Extracting Downloaded Frontend"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>${log_file}
+status_check $?
 
 print_head "Copying Nginx Config for Roboshop"
-
 cp ${code_dir}/config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-
+status_check $?
 
 print_head "Enabling and Starting Nginx"
 systemctl enable nginx &>>${log_file}
 systemctl start nginx  &>>${log_file}
-
+status_check $?
