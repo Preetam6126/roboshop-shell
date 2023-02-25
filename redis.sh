63f6b6cfc874912cb967407f69d9ba22483d59d3
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-25 18:01:49 +0000

    redis app server

---
+++ redis.sh
@@ -0,0 +1,30 @@
+source common.sh
+
+print_head "Installing Redis Repo files"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log_file}
+status_check $?
+
+print_head "Chossing & Enabling module 6.2"
+dnf module enable redis:remi-6.2 -y &>>${log_file}
+status_check $?
+
+print_head "Installing Redis"
+yum install redis -y &>>${log_file}
+status_check $?
+
+print_head "configuring to  Listen to everyone"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
+status_check $?
+
+print_head "Enable System services"
+systemctl enable redis 
+status_check $?
+
+print_head " Start redis system servcies"
+systemctl start redis 
+status_check $?
+
+
+
+
+
