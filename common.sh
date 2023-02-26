595d5392bf2a2a2e4c0a6e8729ef9a45c39d18e8
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-26 12:36:02 +0000

    dry code commonsh

--- common.sh
+++ common.sh
@@ -16,6 +16,25 @@ status_check() {
  fi
  }    
  
+ systemd_setup() {
+  
+print_head "Copying service file to systemd "
+cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+status_check $?
+
+print_head "Systemctl reload"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "Enable ${component} service"
+systemctl enable ${component} &>>${log_file}
+status_check $?
+
+print_head "Start ${component} service"
+systemctl restart ${component} &>>${log_file}
+status_check $?
+ }
+ 
 schema_setup() { 
  if [ "${schema_type}" == "mongo" ]; then
  print_head "Copy mongodb repo files"
@@ -29,19 +48,20 @@ schema_setup() {
  print_head "Load schema to mongodb"
  mongo --host mongodb.devops36.shop </app/schema/${component}.js &>>${log_file}
  status_check $?
- fi
- }
+ elif [ "{$schema_type}" == "mysql" ]; then
  
- NODEJS() {
-  
-print_head "Downloading Repository"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-status_check $?
+ print_head "Install MySql Client"
+ yum install mysql -y &>>${log_file}
+ status_check $?
+ 
+ print_head "Applying password to MySql password and Loading Schema"
+ mysql -h mysql.devops36.shop -uroot -p${mysql_root_password} < /app/schema/shipping.sql &>>${log_file}
+ status_check $?
+}
 
-print_head "Installing Nodejs"
-yum install nodejs -y &>>${log_file} 
-status_check $?
 
+app_prereq_setup() {
+ 
 print_head "Adding User Roboshop"
 id roboshop &>>${log_file}
 if [ $? -ne 0 ]; then
@@ -70,27 +90,49 @@ status_check $?
 print_head "Extracting App content"
 unzip /tmp/${component}.zip &>>${log_file}
 status_check $?
+}
+ 
+NODEJS() {
+  
+print_head "Downloading Repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Installing Nodejs"
+yum install nodejs -y &>>${log_file} 
+status_check $?
+
+app_prereq_setup
 
 print_head "Installing nodejs support files"
 npm install &>>${log_file}
 status_check $?
 
-print_head "Copying service file to systemd "
-cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
-status_check $?
-
-print_head "Systemctl reload"
-systemctl daemon-reload &>>${log_file}
-status_check $?
-
-print_head "Enable ${component} service"
-systemctl enable ${component} &>>${log_file}
-status_check $?
-
-print_head "Start ${component} service"
-systemctl start ${component} &>>${log_file}
-status_check $?
-
 schema_setup
+
+systemd_setup
+}
+
+java() {
+
+print_head "Installing Maven"
+yum install maven -y &>>${log_file}
+status_check $?
+
+app_prereq_setup
+
+print_head "Download Dependencies & Packages"
+mvn clean package &>>${log_file} 
+status_check $?
+
+print_head " Moving files to original name"
+mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+status_check $?
+
+#Schema Set-UP Function
+schema_setup
+
+#SystemD Set-Up Function
+systemd_setup
 
 }
