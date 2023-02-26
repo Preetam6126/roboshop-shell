9c1479fc44b5ab6df3b7f9f4681da68312d0e8a2
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-26 13:05:25 +0000

    dry code

--- common.sh
+++ common.sh
@@ -48,7 +48,7 @@ schema_setup() {
  print_head "Load schema to mongodb"
  mongo --host mongodb.devops36.shop </app/schema/${component}.js &>>${log_file}
  status_check $?
- elif [ "{$schema_type}" == "mysql" ]; then
+ elif [ "${schema_type}" == "mysql" ]; then
  
  print_head "Install MySql Client"
  yum install mysql -y &>>${log_file}
