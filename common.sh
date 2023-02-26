b7b84e6986deebcec7a8f22583f7e04788aee7ce
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-26 01:54:20 +0000

    kept code dry for catalogue and user

--- common.sh
+++ common.sh
@@ -9,9 +9,82 @@ print_head() {
 status_check() {
  if [ $1 -eq 0 ]; then
   echo Success
-else
+ else
   echo Failure
   echo "Read the log file ${log_file} for more information"
   exit 1
+ fi
+ }    
+ 
+ NODEJS() {
+  
+print_head "Downloading Repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Installing Nodejs"
+yum install nodejs -y &>>${log_file} 
+status_check $?
+
+print_head "Adding User Roboshop"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+useradd roboshop &>>${log_file}
 fi
-}    
+status_check $?
+
+print_head "creating Apllication Directory"
+if [ ! -d /app ]; then
+mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "cleaning old files"
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "Downloading App Content zip file"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+status_check $?
+
+print_head "Changing Directory"
+cd /app &>>${log_file}
+status_check $?
+
+print_head "Extracting App content"
+unzip /tmp/${component}.zip &>>${log_file}
+status_check $?
+
+print_head "Installing nodejs support files"
+npm install &>>${log_file}
+status_check $?
+
+print_head "Copying service file to systemd "
+cp ${code_dir}/config/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+status_check $?
+
+print_head "Systemctl reload"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "Enable ${component} service"
+systemctl enable ${component} &>>${log_file}
+status_check $?
+
+print_head "Start ${component} service"
+systemctl start ${component} &>>${log_file}
+status_check $?
+
+
+print_head "Copy mongodb repo files"
+cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
+print_head "Install mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "Load schema to mongodb"
+mongo --host mongodb.devops36.shop </app/schema/${component}.js &>>${log_file}
+status_check $?
+ }
---
+++ userfirstcopy.sh
@@ -0,0 +1,71 @@
+source common.sh
+
+print_head "Downloading Repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Installing Nodejs"
+yum install nodejs -y &>>${log_file} 
+status_check $?
+
+print_head "Adding User Roboshop"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_head "creating Apllication Directory"
+if [ ! -d /app ]; then
+mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "cleaning old files"
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "Downloading App Content zip file"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${log_file}
+status_check $?
+
+print_head "changing Directory"
+cd /app &>>${log_file}
+status_check $?
+
+print_head "Extracting App content"
+unzip /tmp/user.zip &>>${log_file}
+status_check $?
+
+print_head " installing nodejs support files"
+npm install &>>${log_file}
+status_check $?
+
+print_head "copying service file to systemd "
+cp ${code_dir}/config/user.service /etc/systemd/system/user.service &>>${log_file}
+status_check $?
+
+print_head "systemctl reload"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "Enable catalogue service"
+systemctl enable user &>>${log_file}
+status_check $?
+
+print_head "Start catalogue service"
+systemctl start user &>>${log_file}
+status_check $?
+
+
+print_head "copy mongodb repo files"
+cp ${code_dir}/config/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
+print_head "install mongodb"
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "load schema to mongodb"
+mongo --host mongodb.devops36.shop </app/schema/user.js &>>${log_file}
+status_check $?
