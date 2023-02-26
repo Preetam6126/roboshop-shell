6a9979a35c15a5395b61b55989d5da1aadc4a1a6
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-26 12:54:15 +0000

    dry code

--- shipping.sh
+++ shipping.sh
@@ -1,11 +1,12 @@
 source common.sh
 mysql_root_password=$1
-if [ "${mysql_root_password}" == "mysql" ]; then
+
+if [ -z "${mysql_root_password}" ]; then
 echo -e "\e[31mMissing mysql root password argument\e[0m"
 exit 1
 
 fi
 component=shipping
-schema_type='mysql'
+schema_type="mysql"
 java
 
