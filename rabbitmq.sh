609750ec720f8a07e5bb0ff5dc82e184ba0639b4
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-02-26 14:45:38 +0000

    Rabbitmq Server

---
+++ rabbitmq.sh
@@ -0,0 +1,36 @@
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+echo -e "\e[31mMissing mysql root password argument\e[0m"
+exit 1
+fi
+
+
+print_head "Setup Erlang repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${log_file}
+status_check $?
+
+print_head "Setup RabbitMQ"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${log_file}
+status_check $?
+
+print_head "Install Erlang & RabbitMQ"
+yum install rabbitmq-server -y &>>${log_file}
+status_check $?
+
+print_head "Enable RabbitMQ Service"
+systemctl enable rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Start RabbitMQ Server"
+systemctl start rabbitmq-server &>>${log_file}
+status_check $?
+
+print_head "Add Application User"
+rabbitmqctl add_user roboshop ${roboshop_app_password} &>>${log_file}
+status_check $?
+
+print_head "Configure Permissions"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log_file}
+status_check $?
